fba5cfb79a00755b95b6e44192fbb545ab20c308
spavkov <spavkov@gmail.com>
2017-07-23 15:25:04 +0200

    Fixing unity

--- CloudMockApi/App_Start/UnityConfig.cs
+++ CloudMockApi/App_Start/UnityConfig.cs
@@ -1,5 +1,7 @@
 using Microsoft.Practices.Unity;
 using System.Web.Http;
+using CloudMockApi.Library.Configuration;
+using CloudMockApi.Library.Services.Configuration;
 using CloudMockApi.Library.Services.Storage;
 using Unity.WebApi;
 
@@ -14,7 +16,9 @@ namespace CloudMockApi
             // register all your components with the container here
             // it is NOT necessary to register your controllers
             
-            container.RegisterType<ITenantsRepository, TenantsRepository>();
+            container.RegisterType<ITenantsRepository, TenantsRepository>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IConfigurationHelper, ConfigurationHelper>();
+            container.RegisterType<ICloudMockApiStorageConfiguration, CloudMockApiStorageConfiguration>(new ContainerControlledLifetimeManager());
             
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
